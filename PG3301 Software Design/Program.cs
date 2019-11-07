a470b7d0903ed2a62208c456f6a1765829d5675a
zamFe <zamfel18@student.kristiania.no>
2019-11-07 11:58:26 +0100

    Add project files.

---
+++ PG3301 Software Design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG3301_Software_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
