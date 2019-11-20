703061ffcba1ab396bc9e9af2db6a8ccc307d57f
SaeanSavin <andreassaether94@gmail.com>
2019-11-20 22:29:02 +0100

    Set random in marketplace to private

--- PG3301 Software Design/Marketplace.cs
+++ PG3301 Software Design/Marketplace.cs
@@ -11,7 +11,7 @@ namespace PG3301_Software_Design
 
         public ProductDistributor Publisher = new ProductDistributor();
 
-        Random rnd = new Random();
+        private Random rnd = new Random();
 
         public void CreateGame()
         {
